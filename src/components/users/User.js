a993bda4da67684adc718ee56745ebf4a4987bf4
Minh Bui <tranduyminh.bui@gmail.com>
2019-08-06 14:59:39 +0930

    githubFinder with Context API

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,13 @@
-import React, {useEffect, Fragment} from "react";
+import React, {useEffect, Fragment, useContext} from "react";
 import {Link} from "react-router-dom";
 import Spinner from "../layout/Spinner";
 import Repos from "../repos/Repos";
-import PropTypes from "prop-types";
+import GithubContext from "../../context/github/githubContext";
 
-const User = ({user, loading, getUser, getUserRepos, repos, match}) => {
+const User = ({match}) => {
+  const githubContext = useContext(GithubContext);
+  const {getUser, loading, user, repos, getUserRepos} = githubContext;
+
   useEffect(() => {
     getUser(match.params.login);
     getUserRepos(match.params.login);
@@ -101,14 +104,6 @@ const User = ({user, loading, getUser, getUserRepos, repos, match}) => {
       <Repos repos={repos} />
     </Fragment>
   );
-};
-
-User.propTypes = {
-  loading: PropTypes.bool,
-  user: PropTypes.object.isRequired,
-  getUser: PropTypes.func.isRequired,
-  getUserRepos: PropTypes.func.isRequired,
-  repos: PropTypes.array.isRequired
 };
 
 export default User;
